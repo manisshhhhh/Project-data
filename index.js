99fbe0cfe26ccdf99c3100a34ecb968746e83662
Manish <2002mybaran@gmail.com>
2024-03-10 22:18:28 +0530

    bug-fix

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const jsonServer = require("json-server");
-const db = require('./db.json');
-const server = jsonServer.create();
-server.use(jsonServer.defaults());
+const jsonServer = require('json-server')
+const cors = require('cors')
+const path = require('path')
 
-const router = jsonServer.router(db);
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
 
+server.use(cors())
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+server.use(router)
 
-server.use(router);
+const PORT = 8000
 
-server.listen();
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on http://localhost:${PORT}`)
+})
